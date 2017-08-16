f527b0f2ce482ac089adcc8b5c09e60717ef37a9
root <root@JOHN>
2017-08-16 17:11:32 -0600

    commit 02

---
+++ src/main/java/com/mycompany/guia01/utilities.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.guia01;
+
+/**
+ *
+ * @author john
+ */
+public class utilities {
+    public String getResume(String texto){
+texto​ = "Cadena de texto que contiene la introducción de un determinado post";
+return​ "Las primeras 30 letras del texto recibido";}
+public String capitalizar(String texto){
+texto​="Cadena de texto que contiene el título de un determinado post";
+return​  "El texto que se ha recibido con cada palabra capitalizada y sin dobles";
+}
+//public int contarCoincidencias(String frase, String texto){
+//frase​= "Cadena de texto que contiene la frase a buscar";
+//texto​ = "Cadena de texto en donde se buscarán las coincidencias";
+//return​ "La cantidad de coincidencias(case-ignore) de la frase en el texto recibido";//
+//}
+}
