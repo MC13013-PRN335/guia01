3b18d782cb7e4f41eb869651ef8ec19da6d5b48d
root <root@JOHN>
2017-08-16 17:29:21 -0600

    commit 03

--- src/main/java/com/mycompany/guia01/Utilities.java
+++ src/main/java/com/mycompany/guia01/Utilities.java
@@ -1,18 +1,32 @@
 
 package com.mycompany.guia01;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author john
  */
-public class utilities {
+public class Utilities {
+  /**
+   * 
+   * @param texto = "Cadena de texto que contiene la introducción de un determinado post"
+   * @return "Las primeras 30 letras del texto recibido"
+   */  
     public String getResume(String texto){
-texto​ = "Cadena de texto que contiene la introducción de un determinado post";
-return​ "Las primeras 30 letras del texto recibido";}
-public String capitalizar(String texto){
+        System.out.println("");
+    }
+/**
+ * 
+ * @param texto
+ * @return 
+ */
+    public String capitalizar(String texto){
 texto​="Cadena de texto que contiene el título de un determinado post";
 return​  "El texto que se ha recibido con cada palabra capitalizada y sin dobles";
 }
+    
+    
 //public int contarCoincidencias(String frase, String texto){
 //frase​= "Cadena de texto que contiene la frase a buscar";
 //texto​ = "Cadena de texto en donde se buscarán las coincidencias";
