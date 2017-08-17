fbaba89441434ddaf76d3916f3cd2254e45680f6
root <root@JOHN>
2017-08-16 18:26:10 -0600

    commit 03

--- src/main/java/com/mycompany/guia01/Utilities.java
+++ src/main/java/com/mycompany/guia01/Utilities.java
@@ -14,16 +14,29 @@ public class Utilities {
    * @return "Las primeras 30 letras del texto recibido"
    */  
     public String getResume(String texto){
-        System.out.println("");
+      String salida="";
+      if (texto.length() > 30) {
+          for (int i=0; i<=30; i++){
+              salida+= String.valueOf(texto.charAt(i));
+          }
+      }else{
+          salida=texto;
+      } return salida;
     }
 /**
  * 
- * @param texto
- * @return 
+ * @param texto "Cadena de texto que contiene el título de un determinado post";
+
+ * @return "El texto que se ha recibido con cada palabra capitalizada y sin dobles"
  */
     public String capitalizar(String texto){
-texto​="Cadena de texto que contiene el título de un determinado post";
-return​  "El texto que se ha recibido con cada palabra capitalizada y sin dobles";
+String salida;
+texto = texto.replaceAll("", "");
+salida= String.valueOf(texto.charAt(0)).toUpperCase();
+
+for (int i=1 ; i < texto.length() ; i++){
+    if (texto.charAt( i - 1 )==)
+}
 }
     
     
